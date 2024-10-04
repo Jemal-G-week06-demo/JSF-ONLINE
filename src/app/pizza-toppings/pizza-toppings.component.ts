f43ad688c73d17cb34115ae3ff37cb4da2627b69
Jemal-G <jgeda@madisoncollege.edu>
2024-10-03 20:12:34 -0500

    generated pizza toppigs
    command prompt used to generate
    /npm-packages/ng generate component pizza-toppings

---
+++ src/app/pizza-toppings/pizza-toppings.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pizza-toppings',
+  standalone: true,
+  imports: [],
+  templateUrl: './pizza-toppings.component.html',
+  styleUrl: './pizza-toppings.component.css'
+})
+export class PizzaToppingsComponent {
+
+}
